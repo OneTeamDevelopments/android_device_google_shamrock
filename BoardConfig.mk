4296ec16cb21b176713b52c99a89e624311f4156
Wzedlare <vedatak01@gmail.com>
2017-04-17 12:06:16 +0000

    shamrock: Enable WITH_DEXPREOPT := true
    Change-Id: Ifcb548c15b3168d8a8250e5e3006f8afe69e206f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,15 +65,8 @@ TARGET_HW_DISK_ENCRYPTION := true
 BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
 BOARD_USES_CYANOGEN_HARDWARE := true
 
-# Dexopt
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
-    endif
-  endif
-endif
+# Dexpreopt
+WITH_DEXPREOPT := true
 
 # Display
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
