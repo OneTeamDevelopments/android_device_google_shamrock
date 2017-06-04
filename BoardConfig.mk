2ffbd3e9073570ddcd8cb9073f891c100f51a350
wzedlare <vedatak01@gmail.com>
2017-06-04 11:42:43 +0000

    shamrock: Remove dtbtool args
    It is not necessary to indicate dtb args

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,6 @@ TARGET_UNIFIED_DEVICE := true
 TARGET_USES_OVERLAY := true
 
 # Kernel
-BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk 
 BOARD_KERNEL_PAGESIZE := 2048
