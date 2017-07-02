a86b326d13da4eb103b6e55e8524f4a530c23514
Rygebin <kaankulahli2@gmail.com>
2017-07-02 08:43:16 +0000

    shamrock: Build dt.img
    * it's need a twrp build.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,7 @@ TARGET_USES_OVERLAY := true
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk 
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
