8d3e93c1ee0f761bf434fc572d76c9d573fe7be6
Wzedlare <vedatak01@gmail.com>
2017-07-22 18:58:28 +0000

    shamrock: Get blacklist for product model
    * Fixed the device name seen "lineage_shamrock".
    
    ----> Warning! It may cause problems in the compilation of some ROM's.

--- device.mk
+++ device.mk
@@ -203,6 +203,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
+# OEM Blacklist
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
+    ro.product.model \
+
 # Power HAL
 PRODUCT_PACKAGES += \
     power.msm8952
