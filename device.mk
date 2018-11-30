bf25d36176b481467655daa4e386642a2fb6c70f
caneray <punisher_caner@windowslive.com>
2018-11-30 05:13:00 +0300

    Upgrade soundtrigger to 2.1

--- device.mk
+++ device.mk
@@ -75,7 +75,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8952 \
     audio.r_submix.default \
