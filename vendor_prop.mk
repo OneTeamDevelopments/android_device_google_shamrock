cf49aff658f067ab100a2702c6de4ee744c83d27
caneray <punisher_caner@windowslive.com>
2018-12-21 04:53:51 +0300

    shamrock: Add ACDB paths

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -50,6 +50,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.voice.record.conc.disabled=false \
     vendor.voice.voip.conc.disabled=true
 
+# ACDB Paths
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.audio.calfile0=/vendor/etc/acdbdata/QRD/QRD_Bluetooth_cal.acdb \
+    persist.audio.calfile1=/vendor/etc/acdbdata/QRD/QRD_General_cal.acdb \
+    persist.audio.calfile2=/vendor/etc/acdbdata/QRD/QRD_Global_cal.acdb \
+    persist.audio.calfile3=/vendor/etc/acdbdata/QRD/QRD_Handset_cal.acdb \
+    persist.audio.calfile4=/vendor/etc/acdbdata/QRD/QRD_Hdmi_cal.acdb \
+    persist.audio.calfile5=/vendor/etc/acdbdata/QRD/QRD_Headset_cal.acdb \
+    persist.audio.calfile6=/vendor/etc/acdbdata/QRD/QRD_Speaker_cal.acdb
+
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.hfp.client=1 \
