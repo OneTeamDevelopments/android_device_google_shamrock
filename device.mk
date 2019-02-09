52c1ddfe12af540f9cf80bd962251ba9aeb0f991
ZeeLog <Danjel.Zammit@gmail.com>
2019-02-09 16:07:57 +0100

    vince: FM: switch to QCOM FM implementation
    * update FM product packages & properties
    * add QTI FM shell script
    * upate permissions
    * add fm_dl service helper
    * update proprietary files

--- device.mk
+++ device.mk
@@ -199,8 +199,9 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
@@ -344,6 +345,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
+    init.qti.fm.sh \
     init.qcom.post_boot.sh \
     move_time_data.sh \
     move_wifi_data.sh \
