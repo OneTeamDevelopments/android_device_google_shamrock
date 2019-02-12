419216c6f22d0b7ed7110207d78ae22b6ee01840
mdeejay <mdjrussia@gmail.com>
2019-02-12 08:53:27 +0100

    vince: Switch to Snap2

--- device.mk
+++ device.mk
@@ -145,7 +145,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
-    Gcam
+    Snap2
 
 # Consumerir
 PRODUCT_PACKAGES += \
