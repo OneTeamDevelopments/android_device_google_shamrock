c96210ca2182b20f7416f90e071d62aeb2426c53
mdeejay <mdjrussia@gmail.com>
2019-02-16 11:37:00 +0100

    Revert "vince: Switch to Snap2"
    This reverts commit 419216c6f22d0b7ed7110207d78ae22b6ee01840.

--- device.mk
+++ device.mk
@@ -145,7 +145,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
-    Snap2
+    Gcam
 
 # Consumerir
 PRODUCT_PACKAGES += \
