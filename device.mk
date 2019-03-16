8639268aa7b154f2993d1d0430d8938d5b13e184
caneray <punisher_caner@windowslive.com>
2019-03-16 22:29:20 +0000

    shamrock: Use Snap for default camera app

--- device.mk
+++ device.mk
@@ -125,9 +125,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     libmm-qcamera \
-    Camera2 \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    Snap
 
 #Camera Sounds
 PRODUCT_COPY_FILES += \
