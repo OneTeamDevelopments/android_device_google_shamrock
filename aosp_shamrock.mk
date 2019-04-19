d7fad114082e3ea5714d04d2d7eb030fe464fc65
caneray <punisher_caner@windowslive.com>
2019-04-19 00:08:33 +0000

    shamrock: AOSP-ify!

---
+++ aosp_shamrock.mk
@@ -0,0 +1,42 @@
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from shamrock device
+$(call inherit-product, device/google/shamrock/device.mk)
+
+# Inherit some common AOSP stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8952
+
+PRODUCT_NAME := aosp_shamrock
+PRODUCT_DEVICE := shamrock
+PRODUCT_MANUFACTURER := Google
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := GM 5 Plus
+TARGET_VENDOR := Google
+
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="shamrock" \
+    PRODUCT_NAME="shamrock"
