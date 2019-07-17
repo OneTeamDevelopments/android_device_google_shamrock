52338e6284d3d1a482ccb711a887933a5e105aac
caneray <floordarkfire@gmail.com>
2019-07-17 17:09:10 +0000

    shamrock: Fix offline charge

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,7 @@ TARGET_USES_QTI_CAMERA_DEVICE := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # CNE / DPM
 BOARD_USES_QCNE := true
