c1141f1d7f7c733e057240654da0f41e25845f70
Rockzyrain <canemeckisisel@gmail.com>
2019-07-24 13:00:11 +0000

    shamrock: Seamless display flags used

--- device.mk
+++ device.mk
@@ -163,11 +163,7 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libqdMetaData.system \
     libgenlock \
-    libtinyxml \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.1 \
-    vendor.display.config@1.0_vendor \
-    vendor.display.config@1.1_vendor
+    libtinyxml
 
 
 PRODUCT_BOOT_JARS += \
