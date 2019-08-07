8785f19c894305a0e2256264ccc7e07254576c72
caneray <floordarkfire@gmail.com>
2019-08-07 16:01:34 +0300

    shamrock: Support systemwide DT2W

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,7 +189,7 @@ TARGET_PER_MGR_ENABLED := true
 
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
-#TARGET_TAP_TO_WAKE_NODE := "/sys/devices/platform/soc/78b7000.i2c/i2c-3/3-0020/input/input2/wake_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
